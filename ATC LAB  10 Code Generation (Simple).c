bb6e5275b0e7a0c8096d883b1c6647c249c32a5b
Sujan R <24btce238@gcu.edu.in>
2026-03-25 09:06:24 +0530

    Add files via upload

---
+++ ATC LAB  10 Code Generation (Simple).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Expression: a + b\n");
+    printf("Generated Code:\n");
+    printf("LOAD A\n");
+    printf("ADD B\n");
+    printf("STORE RESULT\n");
+    return 0;
+}
